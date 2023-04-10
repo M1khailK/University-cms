bd055250ebdd27c63e88f16e54a62dd381462f1b
Mikhail <korolyuk.mikhail@gmail.com>
2023-04-10 18:43:26 +0300

    University-cms/Added foreign key to createTables.sql and V1_createUniversityTables.sql files

--- src/main/resources/db/migration/V1__createUniversityTables.sql
+++ src/main/resources/db/migration/V1__createUniversityTables.sql
@@ -1,31 +1,39 @@
-create table groups (
+CREATE TABLE groups (
     group_id SERIAL PRIMARY KEY,
     group_name VARCHAR(50) NOT NULL
 );
-create table subjects(
+
+CREATE TABLE subjects (
     subject_id SERIAL PRIMARY KEY,
     subject_name VARCHAR(50) NOT NULL
 );
-create table lessons(
+
+CREATE TABLE teachers (
+    teacher_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE lessons (
     lesson_id SERIAL PRIMARY KEY,
     lesson_name VARCHAR(50) NOT NULL,
     group_id INT,
     subject_id INT,
     teacher_id INT,
+    FOREIGN KEY (group_id) REFERENCES groups(group_id),
+    FOREIGN KEY (subject_id) REFERENCES subjects(subject_id),
+    FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id),
     lesson_date DATE,
     start_time TIME,
     end_time TIME
 );
-create table students(
-	student_id SERIAL PRIMARY KEY,
-	group_id INT,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL
-);
-create table teachers(
-	teacher_id SERIAL PRIMARY KEY,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL
+
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    group_id INT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES groups(group_id)
 );
