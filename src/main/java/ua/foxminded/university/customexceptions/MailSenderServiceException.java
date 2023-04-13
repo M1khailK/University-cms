eb8789ff65181e80b7630306ffab97dcc03644ab
Mikhail <korolyuk.mikhail@gmail.com>
2023-04-13 16:50:33 +0300

    University-cms/MailSenderServiceException extends RuntimeException

--- src/main/java/ua/foxminded/university/customexceptions/MailSenderServiceException.java
+++ src/main/java/ua/foxminded/university/customexceptions/MailSenderServiceException.java
@@ -1,6 +1,6 @@
 package ua.foxminded.university.customexceptions;
 
-public class MailSenderServiceException extends Exception{
+public class MailSenderServiceException extends RuntimeException{
     public MailSenderServiceException(String message,Throwable cause){
         super(message, cause);
     }
