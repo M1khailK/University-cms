77670401f2518da90d08fc1ced90b9edeec0235b
Mikhail <korolyuk.mikhail@gmail.com>
2023-04-22 15:26:22 +0300

    University-cms/GeneralSchedule page was created. Added sidebar menu to all pages.

--- src/main/java/ua/foxminded/university/controller/LoginController.java
+++ src/main/java/ua/foxminded/university/controller/LoginController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomePageController {
+public class LoginController {
 
-    @GetMapping("/")
-    public String homepage() {
-        return "home";
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 
 }
---
+++ src/main/java/ua/foxminded/university/controller/ScheduleController.java
@@ -0,0 +1,69 @@
+package ua.foxminded.university.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ua.foxminded.university.info.Group;
+import ua.foxminded.university.info.Lesson;
+import ua.foxminded.university.info.Student;
+import ua.foxminded.university.info.Teacher;
+import ua.foxminded.university.services.GroupService;
+import ua.foxminded.university.services.LessonService;
+import ua.foxminded.university.services.StudentService;
+import ua.foxminded.university.services.TeacherService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class ScheduleController {
+
+    @Autowired
+    private TeacherService teacherService;
+    @Autowired
+    private GroupService groupService;
+    @Autowired
+    private LessonService lessonService;
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/generalSchedule")
+    public String generalSchedule(Model model) {
+        List<Student> students = studentService.getAll();
+        List<Teacher> teachers = teacherService.getAll();
+        List<Group> groups = groupService.getAll();
+        model.addAttribute("students", students);
+        model.addAttribute("teachers", teachers);
+        model.addAttribute("groups", groups);
+        return "generalSchedule";
+    }
+
+    @PostMapping("/teacherSchedule")
+    public String showTeacherSchedule(Model model, @RequestParam("teacherId") Integer teacherId, @RequestParam("dateFrom") LocalDate dateFrom, @RequestParam("dateTo") LocalDate dateTo) {
+        Teacher teacher = teacherService.getById(teacherId).get();
+        List<Teacher> teachers = teacherService.getAll();
+        List<Student> students = studentService.getAll();
+        List<Lesson> teacherLessons = lessonService.getAllByTeacherAndDateBetween(teacher, dateFrom, dateTo);
+        model.addAttribute("teachers", teachers);
+        model.addAttribute("students", students);
+        model.addAttribute("teacherLessons", teacherLessons);
+        return "generalSchedule";
+    }
+
+    @PostMapping("/studentSchedule")
+    public String showStudentSchedule(Model model, @RequestParam("studentId") Integer studentId, @RequestParam("dateFrom") LocalDate dateFrom, @RequestParam("dateTo") LocalDate dateTo) {
+        Student student = studentService.getById(studentId).get();
+        List<Student> students = studentService.getAll();
+        List<Teacher> teachers = teacherService.getAll();
+
+        List<Lesson> studentLessons = lessonService.getAllByStudentAndDateBetween(student, dateFrom, dateTo);
+        model.addAttribute("students", students);
+        model.addAttribute("teachers", teachers);
+
+        model.addAttribute("studentLessons", studentLessons);
+        return "generalSchedule";
+    }
+}
