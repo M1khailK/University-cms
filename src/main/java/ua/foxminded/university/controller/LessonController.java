c1d4647ede1b33c2080ac10d464727fe20508370
Mikhail <korolyuk.mikhail@gmail.com>
2023-10-27 13:42:40 +0300

    University-cms/Added MapStruct to LessonMapper class

--- src/main/java/ua/foxminded/university/controller/LessonController.java
+++ src/main/java/ua/foxminded/university/controller/LessonController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import ua.foxminded.university.dto.LessonDTO;
 import ua.foxminded.university.info.Lesson;
 import ua.foxminded.university.mapper.LessonMapper;
