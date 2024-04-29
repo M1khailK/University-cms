dad96d006b6fc00db6c4883eabf1c254b66751ba
Mikhail <korolyuk.mikhail@gmail.com>
2024-04-29 12:08:30 +0300

    University-cms/Deleted gradeService from homepage.

--- src/main/java/ua/foxminded/university/controller/HomePageController.java
+++ src/main/java/ua/foxminded/university/controller/HomePageController.java
@@ -1,18 +1,14 @@
 package ua.foxminded.university.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import ua.foxminded.university.services.impl.GradeService;
 
 @Controller
 public class HomePageController {
 
-    @Autowired
-    private GradeService gradeService;
-
     @GetMapping("/")
     public String homepage() {
+
         return "home";
     }
 
