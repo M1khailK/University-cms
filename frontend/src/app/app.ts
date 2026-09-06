4a5de629f947badb706cb5539bdc8906af140133
MikhailK <mikhail.korolyuk@gmail.com>
2026-09-06 20:54:31 +0300

    feat(frontend): add Angular Material

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
-  imports: [RouterOutlet],
+imports: [RouterOutlet, MatToolbarModule],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('university-cms-ui');
 }
